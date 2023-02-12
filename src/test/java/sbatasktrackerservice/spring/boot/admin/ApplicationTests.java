9d183ed5bdcf7cb2b4bd0d0fe1e531356146d545
VladimirKlepikov <radishev95@gmail.com>
2023-02-12 23:31:35 +0700

    Initial commit

---
+++ src/test/java/sbatasktrackerservice/spring/boot/admin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sbatasktrackerservice.spring.boot.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
